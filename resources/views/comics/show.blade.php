b67f0aeda127881a57598b2d62694d8f55a061e4
pietroscandola <pietro.scandolavr@gmail.com>
2022-03-22 17:12:55 +0100

    add show.blade

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,16 +1,20 @@
-@extends('layouts.main');
+@extends('layouts.main')
 
 @section('content')
-<div class="card w-25 m-5">
-    <figure class="d-flex justify-content-center mt-4">
-      <img src="{{ $comic->thumb}}" alt="{{ $comic->series}}">
-    </figure>
-    <div class="card-body">
-      <h5 class="card-title">{{$comic->title}}</h5>
-      <p><strong>Prezzo:</strong>  {{ $comic->price}} €</p>
-      <p><strong>Data di uscita:</strong>  {{ $comic->sale_date}}</p>
-      <a href="{{ route('comics.index', $comic->id)}}" class="btn btn-primary">indietro</a>
+<div class="d-flex justify-content-center">
+    <div class="card w-50 m-5">
+        <figure class="d-flex justify-content-center mt-4">
+          <img class="img-fluid" src="{{ $comic->thumb}}" alt="{{ $comic->series}}">
+        </figure>
+        <div class="card-body">
+          <h5 class="card-title">{{$comic->title}}</h5>
+          <p><strong>Prezzo:</strong>  {{ $comic->price}} €</p>
+          <p><strong>Data di uscita:</strong>  {{ $comic->sale_date}}</p>
+          <p>{{ $comic->description}}</p>
+          <a href="{{ route('comics.index', $comic->id)}}" class="btn btn-primary">Indietro</a>
+        </div>
     </div>
 </div>
+
 
 @endsection
