7cbc45b52f78509c4bd9da70b41377014f6d7fc0
pietroscandola <pietro.scandolavr@gmail.com>
2022-03-23 15:21:25 +0100

    add class active

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,24 +1,24 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="container mt-5">
-  <div class="d-flex justify-content-end mb-3">
-    <a href="{{ route('comics.create')}}" class="btn btn-small btn-success">Aggiungi un Fumetto</a>
-  </div>
-  <div class="cards d-flex flex-wrap justify-content-center">
-    @foreach ($comics as $comic)
-    <div class="card w-25 m-3">
-      <figure class="d-flex justify-content-center mt-4">
-        <img class="img-fluid" src="{{ $comic->thumb}}" alt="{{ $comic->series}}">
-      </figure>
-      <div class="card-body">
-        <h5 class="card-title">{{$comic->title}}</h5>
-        <p><strong>Prezzo:</strong>  {{ $comic->price}} €</p>
-        <p><strong>Data di uscita:</strong>  {{ $comic->sale_date}}</p>
-        <a href="{{ route('comics.show', $comic->id)}}" class="btn btn-primary">Scopri di più</a>
-      </div>
+    <div class="container mt-5">
+        <div class="d-flex justify-content-end mb-3">
+            <a href="{{ route('comics.create') }}" class="btn btn-small btn-success">Aggiungi un Fumetto</a>
+        </div>
+        <div class="cards d-flex flex-wrap justify-content-center">
+            @foreach ($comics as $comic)
+                <div class="card w-25 m-3">
+                    <figure class="d-flex justify-content-center mt-4">
+                        <img class="img-fluid" src="{{ $comic->thumb }}" alt="{{ $comic->series }}">
+                    </figure>
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $comic->title }}</h5>
+                        <p><strong>Prezzo:</strong> {{ $comic->price }} €</p>
+                        <p><strong>Data di uscita:</strong> {{ $comic->sale_date }}</p>
+                        <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-primary">Scopri di più</a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
-    @endforeach
-  </div>
-</div>
 @endsection
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,16 +1,20 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">          
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link{{ Request::is('comic/*')? 'active' : ''}} me-3" href="{{ url('/')}}">HOME</a>
-              </li>              
-              <li class="nav-item">
-                <a class="nav-link" href="{{ route('comics.index')}}">COMICS</a>
-              </li> 
-            </ul>
-          </div>
+        <div class="container">
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link me-3 @if (Request::routeIs('home')) active @endif"
+                            href="{{ url('/') }}">HOME
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link @if (Request::is('comics*')) active @endif"
+                            href="{{ route('comics.index') }}">COMICS
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
 </header>
